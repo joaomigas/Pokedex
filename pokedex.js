edcb55bf73ed8ed6378a34ebeaf9d9797c52a6c6
João Miguel <joaomiguel.galo@hotmail.com>
2022-05-24 14:59:25 -0300

    Terceiro Commit

--- pokedex.js
+++ pokedex.js
@@ -4,17 +4,19 @@ async function getAllPokemons(){
     pokemonArea.setAttribute('class', 'pokemon-area');
     body.appendChild(pokemonArea);
 
-    for(i=1; i<=100; i++){
+    for(i=1; i<=12; i++){
         let pokemons= await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`); 
         let arrayOfPokemons= await pokemons.json();
 
         let pokemon= document.createElement('section');
         pokemon.setAttribute('class', 'pokemon');
+        pokemon.setAttribute('data-pokemon-id', i);
         pokemonArea.appendChild(pokemon);
 
         let pokemonImage= document.createElement('img');
         pokemonImage.setAttribute('class', 'pokemon-image');
         pokemonImage.src= arrayOfPokemons.sprites.front_default;
+        pokemonImage.alt= arrayOfPokemons.species.name;
         pokemon.appendChild(pokemonImage);
 
         let pokemonName= document.createElement('p');
@@ -37,8 +39,29 @@ async function getAllPokemons(){
         power2.innerText= arrayOfPokemons.types[1].type.name;
         pokemonPowers.appendChild(power2);
         }
-        console.log(arrayOfPokemons);
+        console.log(arrayOfPokemons);   
     }
 }
-
 getAllPokemons();
+
+/*
+const closeButton= document.querySelector('.close-button');
+const modalPokemon= document.querySelector('.modal-pokemon')
+closeButton.addEventListener('click', ()=>{
+    modalPokemon.remove();
+});
+
+*/
+
+document.addEventListener('click', (e)=>{
+    if(e.target.classList[0]==="pokemon"){
+    let pokemon= e.target;
+    let idPokemon= pokemon.getAttribute('data-pokemon-id');
+    openInfoPokemons(idPokemon);
+}
+});
+
+function openInfoPokemons(id){
+    console.log(id);
+    //let pokemonId= document.querySelector(`[data-pokemon-id="${numeroDePokemons}"]`);
+}
